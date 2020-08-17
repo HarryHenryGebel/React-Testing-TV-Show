7d753527d0a3f84d7ef23df7147b427d02087194
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-17 15:26:43 -0400

    More readability

--- src/utils/formatSeasons.js
+++ src/utils/formatSeasons.js
@@ -1,10 +1,12 @@
-export const formatSeasons = (allEpisodes) => {
+export default function formatSeasons(allEpisodes) {
   const seasons = {};
   allEpisodes.forEach((episode) => {
+    // create season if this is first episodes
     if (!(`Season ${episode.season}` in seasons)) {
       seasons[`Season ${episode.season}`] = [];
     }
+
     seasons[`Season ${episode.season}`].push(episode);
   });
   return seasons;
-};
+}
